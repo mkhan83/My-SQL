80c37704dbabd6043e08c45bb1143ecfeec881ed
mkhan83 <khanmuneeb1028@gmail.com>
2017-05-01 18:07:27 -0400

    Add files via upload

---
+++ bamazoncustomer.js
@@ -0,0 +1,24 @@
+CREATE DATABASE ProductsDB;
+
+USE ProductsDB;
+
+CREATE TABLE songs(
+  item_id INT NOT NULL,
+  product_name VARCHAR(45) NULL,
+  department_name VARCHAR(45) NULL,
+  Price INT(45) NULL,
+  stock_quantity INT,
+);
+
+INSERT INTO ID (item_id)
+VALUES ('1','22','23','34','223','2232','223','22424');
+
+INSERT INTO (product_name) 
+VALUES ("Television","Basketball", "Foootball", "DVD player", "Shirt");
+
+INSERT INTO (department_name) 
+VALUES ("Sports", "Electronics", "Clothing");
+
+INSERT INTO (Price)
+VALUES ("200", "450", "300","200", "299");
+
